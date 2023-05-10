f643d681b0b27c1a1f5c0f015d3e552248345495
Carlos <carloszambranop11@hotmail.com>
2023-05-10 16:57:45 -0500

    Se agrega el proyecto Musicol Ecommerce

---
+++ server/data/Products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    name: "Velcro Ballerinas For Girls  (Pink)",
+    image: "/images/6.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Velcro Sneakers For Boys & Girls  (Blue)",
+    image: "/images/5.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 599,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Acordeón Hohner Rey",
+    image: "/images/4.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 3599000,
+    countInStock: 5,
+    rating: 4,
+    numReviews: 3,
+  },
+  {
+    name: "Bateria Ludwig JR 5P",
+    image: "/images/3.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 2359000,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    name: "Mixer Behringer X-32",
+    image: "/images/2.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 15999000,
+    countInStock: 7,
+    rating: 5,
+    numReviews: 5,
+  },
+  {
+    name: "Bateria Ludwig Accent 5P",
+    image: "/images/1.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 3449000,
+    countInStock: 3,
+    rating: 5,
+    numReviews: 15,
+  },
+];
+
+export default products;
